c8d749d366ede4964e7df1e1fdb961ea53056de6
Matthew A. Miller <linuxwolf@outer-planes.net>
2019-05-30 15:21:37 -0600

    address code review comments

--- assets/compatcheck.js
+++ assets/compatcheck.js
@@ -1,15 +1,30 @@
-const addonBtn = document.querySelector("a.home__extension-link");
-const uaParams = /Firefox\/(.*)$/.exec(navigator.userAgent);
-if (uaParams) {
-    const ver = parseInt(uaParams[1].split("."[0]));
-    let link, text;
-    if (ver >= 67) {
-        link = "{{ site.addon_link | escape }}";
-        text = "Install for Firefox";
-    } else {
-        link = "{{ site.fx_link | escape }}";
-        text = "Upgrade Firefox";
+---
+---
+document.addEventListener("DOMContentLoaded", () => {
+    const CtoA = {
+        "addon": {
+            link: "{{ site.addon_link | escape }}",
+            text: "Install for Firefox",
+        },
+        "upgrade": {
+            link: "{{ site.fx_link | escape }}",
+            text: "Upgrade Firefox",
+        },
     }
-    addonBtn.href = link;
-    addonBtn.querySelector("p").textContent = text;
-}
+    const addonBtn = document.querySelector("a.home__extension-link");
+    const uaParams = /Firefox\/(.*)$/.exec(navigator.userAgent);
+    if (!uaParams) {
+        return;
+    }
+    const verParams = /(\d+).*$/.exec(uaParams[1]);
+    const ver = parseInt((verParams && verParams[1]) || "0");
+
+    let act;
+    if (ver >= 67) {
+        act = CtoA["addon"];
+    } else {
+        act = CtoA["upgrade"];
+    }
+    addonBtn.href = act.link;
+    addonBtn.querySelector("p").textContent = act.text;
+});
